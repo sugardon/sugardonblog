f10dbfd568ef547605acdc77a1052fd92d482fcf
sugardon <sugar2don@gmail.com>
2022-02-01 01:03:09 +0900

    Add syntax higlight

--- src/pages/components/MDXRemoteComponents/MDXRemoteComponents.tsx
+++ src/pages/components/MDXRemoteComponents/MDXRemoteComponents.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 interface ComponentProps {
   children?: React.ReactChild;
@@ -7,7 +9,25 @@ const h1: React.FC<ComponentProps> = (p: ComponentProps) => {
   return <h1 style={{ color: "red" }}> {p.children} </h1>;
 };
 
+interface CodeProps {
+  children: React.ReactChild;
+  className: string;
+}
+// https://mdxjs.com/guides/syntax-highlighting/#syntax-highlighting-at-run-time
+const code: React.FC<CodeProps> = (p: CodeProps) => {
+  const match = /language-(\w+)/.exec(p.className || "");
+
+  return match ? (
+    <SyntaxHighlighter language={match[1]} style={atomOneDark}>
+      {p.children}
+    </SyntaxHighlighter>
+  ) : (
+    <SyntaxHighlighter style={atomOneDark}>{p.children}</SyntaxHighlighter>
+  );
+};
+
 export const Components = {
   h1: h1,
+  code: code,
 };
 export default Components;
