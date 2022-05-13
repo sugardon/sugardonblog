c47227e9a162b053222dad11408ab3e578e0aa21
sugardon <sugar2don@gmail.com>
2022-05-14 03:40:38 +0900

    Run format

--- src/components/mdx-remote/components.tsx
+++ src/components/mdx-remote/components.tsx
@@ -14,7 +14,7 @@ interface CodeProps {
 // https://mdxjs.com/guides/syntax-highlighting/#syntax-highlighting-at-run-time
 const code = (p: CodeProps) => {
   const match = /language-(\w+)/.exec(p.className || "");
-  const str: string = p.children ? p.children.toString() : ""
+  const str: string = p.children ? p.children.toString() : "";
 
   return match ? (
     <SyntaxHighlighter language={match[1]} style={atomOneDark}>
