80c38e256b3b2ed976baa2f95f3f6161aca3bba2
sugardon <sugar2don@gmail.com>
2022-06-30 01:02:23 +0900

    Add dark mode

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import { DarkSwitcher } from "../../containers/switcher";
 
 const Nav = () => {
   return (
-    <nav className="sticky top-0 w-full bg-gray-50 shadow-sm">
-      <div className="container mx-auto items-center justify-between px-5 py-4 md:py-6">
+    <nav className="sticky top-0 w-full bg-gray-50 shadow-sm dark:bg-gray-900">
+      <div className="container mx-auto flex flex-wrap items-center justify-between px-5 py-4 md:py-6">
         <Link href="/">
           <a className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl">
             sugardon blog
           </a>
         </Link>
+        <div className="flex md:order-2">
+          <DarkSwitcher />
+        </div>
       </div>
     </nav>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import React from "react";
 import "../../styles/global.scss";
 
 export const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 export default App;
