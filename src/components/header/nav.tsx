7f8b96740ef6c5511ad1b6afaa85b0ab46fc0cd2
sugardon <66208081+sugardon@users.noreply.github.com>
2022-06-29 23:37:04 +0900

    Update layout (#83)

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="sticky top-0 w-full bg-gray-50">
+    <nav className="sticky top-0 w-full bg-gray-50 shadow-sm">
       <div className="container mx-auto items-center justify-between px-5 py-4 md:py-6">
         <Link href="/">
           <a className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl">
