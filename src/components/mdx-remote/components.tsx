909c1146d9f15c17207ec36452930365eb93a27d
sugardon <66208081+sugardon@users.noreply.github.com>
2022-07-05 00:32:50 +0900

    Update post (#91)

--- src/components/mdx-remote/components.tsx
+++ src/components/mdx-remote/components.tsx
@@ -1,10 +1,49 @@
+import Link from "next/link";
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
+const getAnchor = (text: string): string => {
+  const encoded = encodeURI(text);
+  return encoded;
+};
 // https://github.com/hashicorp/next-mdx-remote/issues/251
 const h1 = ({ children }: { children?: React.ReactNode }) => {
-  return <h1 className="text-3xl font-bold underline"> {children} </h1>;
+  const anchor = typeof children === "string" ? getAnchor(children) : "";
+  return (
+    <div className="space-y-3">
+      <h1 className="mt-2 text-5xl font-bold" id={anchor}>
+        <Link href={`#${anchor}`}>
+          <a>{children}</a>
+        </Link>
+      </h1>
+      <hr className="border-y-1 dark:border-slate-700" />
+    </div>
+  );
+};
+const h2 = ({ children }: { children?: React.ReactNode }) => {
+  const anchor = typeof children === "string" ? getAnchor(children) : "";
+  return (
+    <div className="space-y-3">
+      <h1 className="mt-2 text-3xl font-bold" id={anchor}>
+        <Link href={`#${anchor}`}>
+          <a>{children}</a>
+        </Link>
+      </h1>
+      <hr className="border-y-1 dark:border-slate-700" />
+    </div>
+  );
+};
+const a = ({ href, ...props }: JSX.IntrinsicElements["a"]) => {
+  return <a className="text-blue-500 hover:underline" href={href} {...props} />;
+};
+
+const ul = (props: JSX.IntrinsicElements["ul"]) => {
+  return <ul className="list-inside list-disc" {...props} />;
+};
+
+const li = ({ children }: JSX.IntrinsicElements["li"]) => {
+  return <li>{children}</li>;
 };
 
 interface CodeProps {
@@ -25,8 +64,13 @@ const code = (p: CodeProps) => {
   );
 };
 
+// https://mdxjs.com/table-of-components/
 export const Components: import("mdx/types").MDXComponents = {
   h1,
+  h2,
+  a,
+  ul,
+  li,
   code,
 };
 export default Components;
--- src/containers/Posts/Title.tsx
+++ src/containers/Posts/Title.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Title: React.FC<Props> = ({ children }: Props) => {
   return (
-    <h1 className="mb-12 text-center text-5xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl">
+    <h1 className="mb-12 text-center text-5xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none">
       {children}
     </h1>
   );
