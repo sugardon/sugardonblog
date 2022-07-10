da2be261032d358135799fd92d66d1c35c43cba8
sugardon <66208081+sugardon@users.noreply.github.com>
2022-07-10 23:30:57 +0900

    Add-devcontainer (#102)

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source $NVM_DIR/nvm.sh
+nvm install
+nvm use
+yarn
+
+# skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
+rm skaffold
+# buildpack
+(curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
