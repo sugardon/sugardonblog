69e23b904cd33e74ab209c4b503b66a6b8cf29f7
sugardon <66208081+sugardon@users.noreply.github.com>
2022-07-10 13:59:39 +0000

    Add pack

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,3 +9,5 @@ yarn
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
 sudo install skaffold /usr/local/bin/
 rm skaffold
+# buildpack
+(curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
