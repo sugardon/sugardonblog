c6fb33501679ae951ad304dd8d5392e880651c37
sugardon <66208081+sugardon@users.noreply.github.com>
2022-07-10 13:22:18 +0000

    Update setup.sh to install skaffold

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,5 +3,9 @@
 source $NVM_DIR/nvm.sh
 nvm install
 nvm use
-
 yarn
+
+# skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
+rm skaffold
