ab2cea9adbdafb7ee71984f2a46b48851b418655
sugardon <sugar2don@gmail.com>
2022-08-25 23:37:45 +0900

    Update nav

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -6,12 +6,16 @@ const Nav = () => {
   // https://flowbite.com/docs/components/navbar/
   return (
     <nav className="sticky top-0 w-full bg-gray-50 shadow-sm dark:bg-gray-900">
-      <div className="container mx-auto flex flex-wrap items-center justify-between px-5 py-4 md:py-6">
-        <Link href="/">
-          <a className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl">
-            sugardon blog
-          </a>
-        </Link>
+      <div className="max-w-8xl container mx-auto flex items-center justify-between px-5 py-4 md:py-6">
+        {/* logo */}
+        <div className="flex-none md:order-1">
+          <Link href="/">
+            <a className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl">
+              sugardon blog
+            </a>
+          </Link>
+        </div>
+        {/* dark switcher */}
         <div className="flex md:order-2">
           <DarkSwitcher />
         </div>
