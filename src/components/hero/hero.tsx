8a13fd22919453a54d924d8a2dd5b68011ec7709
sugardon <sugar2don@gmail.com>
2022-09-11 23:21:26 +0900

    Update image

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -9,35 +9,31 @@ const Hero: React.FC = () => {
         </p>
 
         <h1 className="text-black-800 mb-8 text-4xl font-bold sm:text-5xl md:mb-12 md:text-6xl">
-          sugardon blog
+          blog
         </h1>
-
-        <p className="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg">
-          articles
-        </p>
 
         <div className="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
           <a
             href="#"
             className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
           >
-            Start now
+            About
           </a>
 
           <a
             href="#"
             className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
           >
-            Take tour
+            articles
           </a>
         </div>
       </div>
 
       <div className="h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:h-auto xl:w-5/12">
         <img
-          src="https://images.unsplash.com/photo-1618004912476-29818d81ae2e?auto=format&q=75&fit=crop&w=1000"
+          src="./sugardon.webp"
           loading="lazy"
-          alt="Photo by Fakurian Design"
+          alt="sugardon"
           className="h-full w-full object-cover object-center"
         />
       </div>
