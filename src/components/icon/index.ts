d298a7420924fd5559319587e9dd8a1afb29d229
sugardon <sugar2don@gmail.com>
2022-10-10 23:14:43 +0900

    Refactor menu icons

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,3 +1,5 @@
 export { Link as LinkIcon } from "./heroicons";
 export { Dark as DarkIcon } from "./heroicons";
 export { Sun as SunIcon } from "./heroicons";
+export { Menu as MenuIcon } from "./heroicons";
+export { X as XIcon } from "./heroicons";
