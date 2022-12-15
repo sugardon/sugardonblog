37c06c5dfe571d462087a54fe6a8cac932d598ae
sugardon <66208081+sugardon@users.noreply.github.com>
2022-12-16 00:53:20 +0900

    Update mermaid for dark theme (#261)

--- src/components/mdx-remote/mermaid.tsx
+++ src/components/mdx-remote/mermaid.tsx
@@ -1,4 +1,5 @@
 import mermaid from "mermaid";
+import { useTheme } from "next-themes";
 import React, { useEffect, useRef } from "react";
 
 // https://github.com/vercel/next.js/discussions/12837
@@ -6,10 +7,12 @@ export const Mermaid: React.FC<{ children?: React.ReactNode }> = ({
   children,
 }) => {
   const elmRef = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
 
   useEffect(() => {
     mermaid.initialize({
       startOnLoad: false,
+      theme: theme === "dark" ? "dark" : "default",
     });
     const elm = elmRef.current;
     if (!elm) return;
